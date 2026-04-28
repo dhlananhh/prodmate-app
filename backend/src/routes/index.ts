3efc172cdca19ad665ecb5dd15646edce5a0196a
dhlananhh <dhlananh2309@gmail.com>
2026-04-28 21:04:29 +0700

    chore(routes): add auth routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import todoRoutes from "./todo.routes";
 import habitRoutes from "./habit.routes";
 import eventRoutes from "./event.routes";
+import authRoutes from "./auth.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ const router = Router();
 router.use("/todos", todoRoutes);
 router.use("/habits", habitRoutes);
 router.use("/events", eventRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
