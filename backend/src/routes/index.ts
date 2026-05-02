c5cc40e5ea0de99ec4276826a85ba9275d5b8c25
dhlananhh <dhlananh2309@gmail.com>
2026-05-02 17:14:26 +0700

    feat(routes): add jwks and user routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 
+
 import todoRoutes from "./todo.routes";
 import habitRoutes from "./habit.routes";
 import eventRoutes from "./event.routes";
 import authRoutes from "./auth.routes";
+import jwksRoutes from "./jwks.routes";
+import userRoutes from "./user.routes";
+
 
 const router = Router();
 
 // Mount each route under its base path
+router.use("/", jwksRoutes);
 router.use("/todos", todoRoutes);
 router.use("/habits", habitRoutes);
 router.use("/events", eventRoutes);
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+
 
 export default router;
