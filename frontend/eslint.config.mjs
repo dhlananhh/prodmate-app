4d7c6576cec2eb9c4848530d3f4b5bac733da278
dhlananhh <dhlananh2309@gmail.com>
2026-05-04 00:55:39 +0700

    chore(frontend): add eslint configuration file

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,17 +2,48 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+
 const eslintConfig = defineConfig([
+  // Inherit the default configuration of Next.js
   ...nextVitals,
   ...nextTs,
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    "node_modules/**",
     ".next/**",
+    ".husky/**",
     "out/**",
     "build/**",
+    "public/**",
     "next-env.d.ts",
+    "globals.d.ts",
   ]),
+
+  // Additional custom configurations
+  {
+    parser: "@typescript-eslint/parser",
+    plugins: [ "@typescript-eslint", "react" ],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "prettier"
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "react/jsx-curly-spacing": [
+        "error",
+        {
+          when: "never",
+          children: true
+        }
+      ]
+    }
+  }
 ]);
+
 
 export default eslintConfig;
